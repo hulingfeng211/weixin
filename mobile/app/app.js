c675d87940f7f4217277105935b1ec9e5d6a8a27
hulingfeng211 <hulingfeng211@163.com>
2015-10-29 15:02:56 +0800

    add new app to project.

--- mobile/app/app.js
+++ mobile/app/app.js
@@ -27,8 +27,9 @@ app.run(function($transform) {
 app.config(function($routeProvider) {
   $routeProvider.when('/',              {templateUrl: 'home.html', reloadOnSearch: false});
   $routeProvider.when('/scroll',        {templateUrl: 'scroll.html', reloadOnSearch: false});
-  $routeProvider.when('/toggle',        {templateUrl: 'toggle.html', reloadOnSearch: false}); 
-  $routeProvider.when('/tabs',          {templateUrl: 'tabs.html', reloadOnSearch: false}); 
+  $routeProvider.when('/toggle',        {templateUrl: 'toggle.html', reloadOnSearch: false});
+  $routeProvider.when('/contactus',        {templateUrl: 'contactus.html', reloadOnSearch: false});
+ /* $routeProvider.when('/tabs',          {templateUrl: 'tabs.html', reloadOnSearch: false});
   $routeProvider.when('/accordion',     {templateUrl: 'accordion.html', reloadOnSearch: false}); 
   $routeProvider.when('/overlay',       {templateUrl: 'overlay.html', reloadOnSearch: false}); 
   $routeProvider.when('/forms',         {templateUrl: 'forms.html', reloadOnSearch: false});
@@ -37,7 +38,7 @@ app.config(function($routeProvider) {
   $routeProvider.when('/swipe',         {templateUrl: 'swipe.html', reloadOnSearch: false});
   $routeProvider.when('/drag',          {templateUrl: 'drag.html', reloadOnSearch: false});
   $routeProvider.when('/drag2',         {templateUrl: 'drag2.html', reloadOnSearch: false});
-  $routeProvider.when('/carousel',      {templateUrl: 'carousel.html', reloadOnSearch: false});
+  $routeProvider.when('/carousel',      {templateUrl: 'carousel.html', reloadOnSearch: false});*/
 });
 
 // 
